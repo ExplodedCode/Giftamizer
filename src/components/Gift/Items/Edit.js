6517272320498b54d23cfa7fcbbe837b92cfcb4c
Evan Trowbridge <evan@trowbridge.tech>
2021-06-20 20:09:04 -0400

    fix url detection

--- src/components/Gift/Items/Edit.js
+++ src/components/Gift/Items/Edit.js
@@ -78,7 +78,7 @@ export default function SpeedDialTooltipOpen(props) {
 							setLoadingMetadata(true);
 							var urlQuery = e.clipboardData.getData('Text');
 
-							fetch('https://api.giftamizer.com/api/metadata?url=' + urlQuery, {
+							fetch('https://giftamizer.com/api/metadata?url=' + urlQuery, {
 								method: 'GET',
 								headers: {
 									'Content-Type': 'application/json',
