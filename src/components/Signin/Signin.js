76652678742597d914f7f12dce7a8483b3583b60
Evan Trowbridge <etrowbridge@pennair.com>
2021-11-07 14:51:52 -0500

    added email type attribute to login/signup

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -89,6 +89,7 @@ export default function SignInSide() {
 						required
 						fullWidth
 						label='Email Address'
+						type='email'
 						autoComplete='email'
 						autoFocus
 						value={email}
