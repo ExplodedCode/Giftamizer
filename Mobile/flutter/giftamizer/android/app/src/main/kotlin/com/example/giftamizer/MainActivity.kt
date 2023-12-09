a15a61e524bf89538ddcc5e063482e703e553a46
EvanTrow <evan@trowbridge.tech>
2023-12-09 12:47:29 -0500

    clean up

--- Mobile/flutter/giftamizer/android/app/src/main/kotlin/com/example/giftamizer/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.giftamizer.giftamizer
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ proxy.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+app.use(
+	createProxyMiddleware(['/auth', '/rest', '/realtime', '/storage', '/functions', '/analytics', '/pg'], {
+		target: 'http://192.168.1.50:8000',
+		changeOrigin: true,
+		ws: true,
+	})
+);
+
+// supabase studio
+app.use(createProxyMiddleware(['/project', '/monaco-editor', '/css', '/_next', '/api', '/favicon', '/img'], { target: 'http://192.168.1.50:3000', changeOrigin: true, ws: true }));
+
+app.use(createProxyMiddleware('/', { target: 'http://localhost:443', changeOrigin: true, ws: true }));
+
+app.listen(3000);
